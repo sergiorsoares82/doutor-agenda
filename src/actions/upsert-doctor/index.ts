3a743dc4d3d85337f6a6a7779ae096f642065d11
Sergio Soares <sergio.veiga.soares@gmail.com>
2025-06-01 19:22:52 -0300

    feat: implement DoctorCard component and availability helper, add currency formatting utility

--- src/actions/upsert-doctor/index.ts
+++ src/actions/upsert-doctor/index.ts
@@ -1,6 +1,7 @@
 "use server";
 import dayjs from "dayjs";
 import utc from "dayjs/plugin/utc";
+import { revalidatePath } from "next/cache";
 import { headers } from "next/headers";
 
 import { db } from "@/db";
@@ -55,4 +56,6 @@ export const upsertDoctor = actionClient
           availableToTime: availableToTimeUTC.format("HH:mm:ss"),
         },
       });
+
+    revalidatePath("/doctors");
   });
